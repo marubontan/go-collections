ec2e6b235beeef38d2a0b2881b9a2d0ab68e2ddc
marubontan <marubontan@gmail.com>
2023-12-13 21:46:32 -0300

    add counter

---
+++ counter/counter.go
@@ -0,0 +1,37 @@
+package counter
+
+import (
+	"errors"
+	"sort"
+)
+
+type Counter[T comparable] struct {
+	data map[T]int
+}
+
+func NewCounter[T comparable](data []T) (Counter[T], error) {
+	if len(data) == 0 {
+		return Counter[T]{data: make(map[T]int)}, errors.New("empty data")
+
+	}
+	count := make(map[T]int)
+	for _, v := range data {
+		count[v] += 1
+	}
+	return Counter[T]{data: count}, nil
+}
+
+func (c *Counter[T]) MostCommon(n int) []map[T]int {
+	keys := make([]T, 0, len(c.data))
+	for k, _ := range c.data {
+		keys = append(keys, k)
+	}
+	sort.SliceStable(keys, func(i, j int) bool {
+		return c.data[keys[i]] > c.data[keys[j]]
+	})
+	commonItems := make([]map[T]int, 0, n)
+	for _, key := range keys[:n] {
+		commonItems = append(commonItems, map[T]int{key: c.data[key]})
+	}
+	return commonItems
+}
---
+++ counter/counter_test.go
@@ -0,0 +1,24 @@
+package counter
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntCounter(t *testing.T) {
+	counter, _ := NewCounter([]int{1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5})
+	assert.Equal(t, []map[int]int{{5: 5}, {4: 4}}, counter.MostCommon(2))
+}
+
+func TestStringCounter(t *testing.T) {
+	counter, _ := NewCounter([]string{"1", "2", "2", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "5"})
+	assert.Equal(t, []map[string]int{{"5": 5}, {"4": 4}}, counter.MostCommon(2))
+}
+
+func TestEmptyCounter(t *testing.T) {
+	_, err := NewCounter([]int{})
+	assert.Equal(t, err, errors.New("empty data"))
+
+}
